c7211c17102833d1445efd579911acbc054d9417
darknetUzb <101189497+darknetUzb@users.noreply.github.com>
2022-05-08 22:08:40 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+import banner
+import text
+import magicbot2
+import os, sys
+os.system("apt update && apt upgrade && pkg install git && pkg install python && pip install telethon && pip install base64 && git clone https://github.com/darknetUzb/MagicBot-2 && cd MagicBot-2 && python magicbot2.py")
+print(banner)
+print(text)
+print(magicbot2)
---
+++ text.py
@@ -0,0 +1,11 @@
+print("""\033[1;34m
+MagicBot-v2 
+18 xil animatsia bor hozircha
+harhafta 20 xil animatsia qoshiladi
+animatsiadan foydalanish qoidalari
+
+1) Hech qachon 1-animatsia tugamasidan boshqa animatsiani qollamang 
+2) birdaniga kop marta Takrorlamang
+
+agar shu woidaga amal qilsangiz akauntingiz ban ham flood ham bolmaydi
+""")
